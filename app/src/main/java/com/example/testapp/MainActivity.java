3e7e5f4c39a2889e0ab9ddbff0ec249a3ad571e7
Javier Miranda <jmiranda@iuma.ulpgc.es>
2020-02-12 12:25:37 +0000

    Updating server code

--- app/src/main/java/com/example/testapp/MainActivity.java
+++ app/src/main/java/com/example/testapp/MainActivity.java
@@ -6,7 +6,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-    // Documentacion inicial
+    // Documentacion inicial: $Date
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
